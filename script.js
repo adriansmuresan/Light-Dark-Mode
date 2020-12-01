c481f8f1e8681b7e034f7b5c7373f2e96962b7b7
Adrian S MUresan <adriansmuresan@gmail.com>
2020-12-01 16:27:41 -0600

    Implements local storage in order to make the theme persist

--- script.js
+++ script.js
@@ -35,12 +35,25 @@ function switchTheme(event) {
   // console.log(event.target.checked);
   if(event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
     darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
     lightMode();
   }
 }
 
 // Event listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for theme
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if(currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
